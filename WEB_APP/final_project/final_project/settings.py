b3b70dd346bc5a89342d341d5bbb0631b859b3e2
MyriamLbhn <lebihan.myriam@gmail.com>
2023-12-05 09:41:44 +0100

    Ajout du Dockerfile + gestion SECRET KEY

--- WEB_APP/final_project/final_project/settings.py
+++ WEB_APP/final_project/final_project/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+# Chargez les variables d'environnement à partir du fichier .env
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-083_g+!e74b1z3of)k3*8)a+95umda^rnw5cb$q&xrf+jks3@f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'lebihan-django-azure-app.azurewebsites.net']
 
 
 # Application definition
